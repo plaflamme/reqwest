ebad239a307f7ed897d7cffd329e4f351d3ace5f
Philippe Laflamme <philippe@hopper.com>
2022-11-21 15:47:03 -0500

    feat: add `headers` helper method to `ResponseBuilderExt`
    This is analogous to the `headers` method on `RequestBuilder`.

--- src/response.rs
+++ src/response.rs
@@ -1,3 +1,4 @@
+use http::HeaderMap;
 use url::Url;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -10,18 +11,32 @@ pub trait ResponseBuilderExt {
     /// A builder method for the `http::response::Builder` type that allows the user to add a `Url`
     /// to the `http::Response`
     fn url(self, url: Url) -> Self;
+
+    /// Add a set of Headers to the existing ones on this `http::Response`.
+    ///
+    /// The headers will be merged in to any already set.
+    fn headers(self, headers: HeaderMap) -> Self;
 }
 
 impl ResponseBuilderExt for http::response::Builder {
     fn url(self, url: Url) -> Self {
         self.extension(ResponseUrl(url))
     }
+
+    fn headers(mut self, headers: HeaderMap) -> Self {
+        if let Some(target) = self.headers_mut() {
+            crate::util::replace_headers(target, headers);
+        }
+        self
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::iter::FromIterator;
+
     use super::{ResponseBuilderExt, ResponseUrl};
-    use http::response::Builder;
+    use http::{response::Builder, HeaderMap, HeaderValue};
     use url::Url;
 
     #[test]
@@ -37,5 +52,30 @@ mod tests {
             response.extensions().get::<ResponseUrl>(),
             Some(&ResponseUrl(url))
         );
+    }
+
+    #[test]
+    fn test_response_builder_ext_headers() {
+        let response = Builder::new()
+            .status(200)
+            .header(hyper::header::CONTENT_TYPE, "application/json")
+            .header(hyper::header::CONTENT_LENGTH, 42)
+            .headers(HeaderMap::from_iter([
+                (hyper::header::CONTENT_TYPE, HeaderValue::from_static("xyz")),
+                (hyper::header::ETAG, HeaderValue::from_static("abcd")),
+            ]))
+            .body(())
+            .unwrap();
+
+        let expected = [
+            (hyper::header::CONTENT_TYPE, HeaderValue::from_static("xyz")),
+            (
+                hyper::header::CONTENT_LENGTH,
+                HeaderValue::from_static("42"),
+            ),
+            (hyper::header::ETAG, HeaderValue::from_static("abcd")),
+        ];
+
+        assert_eq!(response.headers(), &HeaderMap::from_iter(expected));
     }
 }
